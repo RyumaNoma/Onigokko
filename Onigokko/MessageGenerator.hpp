31b4ae58fcc9b56ea3b506897127d4d929ced614
RyumaNoma <noma11.develop@gmail.com>
2023-09-03 11:48:37 +0900

    [add] MessageGeneratorクラスのヘッダ定義を追加

--- Onigokko/MessageGenerator.hpp
+++ Onigokko/MessageGenerator.hpp
@@ -1,6 +1,25 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <sstream>
 
 class MessageGenerator
 {
+public:
+	MessageGenerator();
+	std::string generate() const;
+
+	void setDestination(std::string destination, int id);
+	void setDestinationBroadCast(std::string destination);
+
+	bool isBroadCast() const;
+	int getDestinationId() const { return _destRange; }
+	const std::string& getSignature() const { return _signature; }
+	const std::vector<std::string>& getArguments() const { return _arguments; }
+private:
+	std::string _destTag;
+	int _destRange;
+	std::string _signature;
+	std::vector<std::string> _arguments;
 };
 
