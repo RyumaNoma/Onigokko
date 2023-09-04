78e130e46a2395ec7b9ba9aa10d6812a3371f9d1
RyumaNoma <noma11.develop@gmail.com>
2023-09-04 13:54:27 +0900

    [change] MessageGenerator -> game::MessageGenerator

--- Onigokko/MessageGenerator.cpp
+++ Onigokko/MessageGenerator.cpp
@@ -3,73 +3,74 @@
 #include <cctype>
 #define DESTINATION_BROADCAST (-1)
 
-MessageGenerator::MessageGenerator()
-: _destTag("")
-, _destRange(0)
-, _signature("")
-, _arguments()
-{}
+namespace game {
+	MessageGenerator::MessageGenerator()
+	: _destTag("")
+	, _destRange(0)
+	, _signature("")
+	, _arguments()
+	{}
 
-std::string MessageGenerator::generate() const {
-	std::ostringstream oss;
-	// 宛先タグ1
-	if (_destTag.empty()) { throw std::runtime_error("empty destination tag"); }
-	oss << '[' << _destTag << ']';
-	// 宛先タグ2
-	if (_destRange == -1) {
-		oss << "[Broadcast]";
-	}
-	else {
-		oss << '[' << _destRange << ']';
-	}
-	// シグネチャ
-	if (_signature.empty()) { throw std::runtime_error("empty signature"); }
-	oss << _signature;
-	// 引数
-	oss << '(';
-	if (!_arguments.empty()) {
-		for (int i = 0; i < _arguments.size() - 1; ++i) {
-			oss << _arguments[i] << ',';
+	std::string MessageGenerator::generate() const {
+		std::ostringstream oss;
+		// 宛先タグ1
+		if (_destTag.empty()) { throw std::runtime_error("empty destination tag"); }
+		oss << '[' << _destTag << ']';
+		// 宛先タグ2
+		if (_destRange == -1) {
+			oss << "[Broadcast]";
 		}
-		oss << _arguments.back();
-	}
-	oss << ')';
-
-	return oss.str();
-}
-
-void MessageGenerator::setDestination(const std::string& destination, int id) {
-	if (destination.empty()) { throw std::invalid_argument("empty destination tag"); }
-	if (id < 0) { throw std::invalid_argument("minus destination id"); }
-
-	_destTag = destination;
-	_destRange = id;
-}
-
-void MessageGenerator::setDestinationBroadcast(const std::string& destination) {
-	if (destination.empty()) { throw std::invalid_argument("empty destination tag"); }
-
-	_destTag = destination;
-	_destRange = -1;
-}
-
-void MessageGenerator::setSignature(const std::string& signature) {
-	if (signature.empty()) { throw std::invalid_argument("empty signature"); }
-	for (char c : signature) {
-		if (!isValidSignatureCharacter(c)) {
-			throw std::invalid_argument("signature include bad character");
+		else {
+			oss << '[' << _destRange << ']';
 		}
+		// シグネチャ
+		if (_signature.empty()) { throw std::runtime_error("empty signature"); }
+		oss << _signature;
+		// 引数
+		oss << '(';
+		if (!_arguments.empty()) {
+			for (int i = 0; i < _arguments.size() - 1; ++i) {
+				oss << _arguments[i] << ',';
+			}
+			oss << _arguments.back();
+		}
+		oss << ')';
+
+		return oss.str();
 	}
 
-	_signature = signature;
-}
+	void MessageGenerator::setDestination(const std::string& destination, int id) {
+		if (destination.empty()) { throw std::invalid_argument("empty destination tag"); }
+		if (id < 0) { throw std::invalid_argument("minus destination id"); }
 
-bool MessageGenerator::isBroadcast() const {
-	return (_destRange == DESTINATION_BROADCAST);
-}
+		_destTag = destination;
+		_destRange = id;
+	}
 
-bool MessageGenerator::isValidSignatureCharacter(char c) const
-{
-	return (std::isdigit(c) || std::isalpha(c) || c == '.' || c == '_' || c == '-');
-}
+	void MessageGenerator::setDestinationBroadcast(const std::string& destination) {
+		if (destination.empty()) { throw std::invalid_argument("empty destination tag"); }
 
+		_destTag = destination;
+		_destRange = -1;
+	}
+
+	void MessageGenerator::setSignature(const std::string& signature) {
+		if (signature.empty()) { throw std::invalid_argument("empty signature"); }
+		for (char c : signature) {
+			if (!isValidSignatureCharacter(c)) {
+				throw std::invalid_argument("signature include bad character");
+			}
+		}
+
+		_signature = signature;
+	}
+
+	bool MessageGenerator::isBroadcast() const {
+		return (_destRange == DESTINATION_BROADCAST);
+	}
+
+	bool MessageGenerator::isValidSignatureCharacter(char c) const
+	{
+		return (std::isdigit(c) || std::isalpha(c) || c == '.' || c == '_' || c == '-');
+	}
+}
--- Onigokko/MessageGenerator.hpp
+++ Onigokko/MessageGenerator.hpp
@@ -3,46 +3,48 @@
 #include <string>
 #include <sstream>
 
-class MessageGenerator
-{
-public:
-	MessageGenerator();
-	std::string generate() const;
+namespace game {
+	class MessageGenerator
+	{
+	public:
+		MessageGenerator();
+		std::string generate() const;
 
-	void setDestination(const std::string& destination, int id);
-	void setDestinationBroadcast(const std::string& destination);
-	void setSignature(const std::string& signature);
+		void setDestination(const std::string& destination, int id);
+		void setDestinationBroadcast(const std::string& destination);
+		void setSignature(const std::string& signature);
+		template<class ArgType>
+		void addArgument(const ArgType& argument);
+		template<class ArgType>
+		void updateArgument(int index, const ArgType& newArgument);
+
+		bool isBroadcast() const;
+		int getDestinationId() const { return _destRange; }
+		const std::string& getSignature() const { return _signature; }
+		const std::string& getArgument(int index) const { return _arguments.at(index); }
+		const std::vector<std::string>& getArguments() const { return _arguments; }
+	private:
+		bool isValidSignatureCharacter(char c) const;
+	private:
+		std::string _destTag;
+		int _destRange;
+		std::string _signature;
+		std::vector<std::string> _arguments;
+	};
+
 	template<class ArgType>
-	void addArgument(const ArgType& argument);
+	inline void MessageGenerator::addArgument(const ArgType& argument) {
+		std::ostringstream oss;
+		oss << argument;
+
+		_arguments.emplace_back(oss.str());
+	}
+
 	template<class ArgType>
-	void updateArgument(int index, const ArgType& newArgument);
+	inline void MessageGenerator::updateArgument(int index, const ArgType& newArgument) {
+		std::ostringstream oss;
+		oss << newArgument;
 
-	bool isBroadcast() const;
-	int getDestinationId() const { return _destRange; }
-	const std::string& getSignature() const { return _signature; }
-	const std::string& getArgument(int index) const { return _arguments.at(index); }
-	const std::vector<std::string>& getArguments() const { return _arguments; }
-private:
-	bool isValidSignatureCharacter(char c) const;
-private:
-	std::string _destTag;
-	int _destRange;
-	std::string _signature;
-	std::vector<std::string> _arguments;
-};
-
-template<class ArgType>
-inline void MessageGenerator::addArgument(const ArgType& argument) {
-	std::ostringstream oss;
-	oss << argument;
-
-	_arguments.emplace_back(oss.str());
-}
-
-template<class ArgType>
-inline void MessageGenerator::updateArgument(int index, const ArgType& newArgument) {
-	std::ostringstream oss;
-	oss << newArgument;
-
-	_arguments.at(index) = oss.str();
+		_arguments.at(index) = oss.str();
+	}
 }
