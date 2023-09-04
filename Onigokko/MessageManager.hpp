decb7047c9324025f9122b3318ae7026baf129b9
RyumaNoma <noma11.develop@gmail.com>
2023-09-04 22:18:28 +0900

    [add] MessageManagerの定義を追加

--- Onigokko/MessageManager.hpp
+++ Onigokko/MessageManager.hpp
@@ -1,5 +1,44 @@
 #pragma once
-class MessageManager
-{
-};
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <memory>
 
+namespace game {
+	class MessageCommunicator;
+	using MessageCommunicatorPtr = std::shared_ptr<MessageCommunicator>;
+
+	class MessageManager {
+		using CommunicatorHierarchy = 
+			std::unordered_map<std::string,
+							   std::unordered_map<int,
+												  std::vector<MessageCommunicatorPtr>>>;
+	public:
+		MessageManager();
+
+		void setCommunicator(const std::string& tag, int id, MessageCommunicatorPtr communicator);
+		void receive(const std::string& message);
+		void sendAll();
+
+		const std::vector<std::string>& getMessages() const { return _messages; }
+		const CommunicatorHierarchy& getCommunicators() const { return _communicators; }
+
+		// –³‚¯‚ê‚Înullptr‚ð•Ô‚·
+		MessageCommunicatorPtr getCommunicator(const std::string& tag, int id) const;
+	private:
+		std::vector<std::string> _messages;
+		/*
+		ŽóMŽÒ‚ÌŠK‘w\‘¢
+		tag -> id
+		ex)
+		- enemy
+			- 0
+			- 1
+		- immutableObject
+			- 0
+			- 2
+		*/
+		CommunicatorHierarchy _communicators;
+	};
+	using MessageManagerPtr = std::shared_ptr<MessageManager>;
+}
