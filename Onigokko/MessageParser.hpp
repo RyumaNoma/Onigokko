26a344f880a5dac91d2c848e58b0ffa8e2c35b6c
RyumaNoma <noma11.develop@gmail.com>
2023-09-04 14:31:58 +0900

    [change] MesageParserをgame名前空間に追加

--- Onigokko/MessageParser.hpp
+++ Onigokko/MessageParser.hpp
@@ -2,25 +2,26 @@
 #include <string>
 #include <vector>
 
-class MessageParser
-{
-public:
-	MessageParser() = delete;
-	MessageParser(const std::string& message);
+namespace game {
+	class MessageParser
+	{
+	public:
+		MessageParser() = delete;
+		MessageParser(const std::string& message);
 	
-	std::string getDestination() const;
-	int getDestinationId() const;
-	bool isBroadcast() const;
-	std::string getSignature() const;
-	std::string getArgumentAsString(int index) const;
-	int getArgumentAsInteger(int index) const;
-	double getArgumentAsDouble(int index) const;
-private:
-	void parse(const std::string& message);
-private:
-	std::string _destTag;
-	int _destRange;
-	std::string _signature;
-	std::vector<std::string> _arguments;
-};
-
+		std::string getDestination() const;
+		int getDestinationId() const;
+		bool isBroadcast() const;
+		std::string getSignature() const;
+		std::string getArgumentAsString(int index) const;
+		int getArgumentAsInteger(int index) const;
+		double getArgumentAsDouble(int index) const;
+	private:
+		void parse(const std::string& message);
+	private:
+		std::string _destTag;
+		int _destRange;
+		std::string _signature;
+		std::vector<std::string> _arguments;
+	};
+}
