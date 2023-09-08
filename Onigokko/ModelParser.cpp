f8feddc3ae70fe5ecfdd2e5cdb4510122301f07d
RyumaNoma <noma11.develop@gmail.com>
2023-09-08 11:49:50 +0900

    [add] ModelParserクラスの追加

---
+++ Onigokko/ModelParser.cpp
@@ -0,0 +1,1 @@
+#include "ModelParser.hpp"
---
+++ Onigokko/ModelParser.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "DxLib.h"
+#include <string>
+#include <vector>
+#include <tuple>
+
+class ModelParser {
+public:
+	ModelParser();
+	ModelParser(const std::string& filename);
+
+	const std::vector<VERTEX3D>& getVertexBuffer() const { return _vertexBuffer; }
+	const std::vector<std::vector<unsigned short>>& getIndexBuffer() const { return _indexBuffer; }
+private:
+	void parse(const std::string& filename);
+private:
+	std::vector<VERTEX3D> _vertexBuffer;
+	std::vector<std::vector<unsigned short>> _indexBuffer;
+};
