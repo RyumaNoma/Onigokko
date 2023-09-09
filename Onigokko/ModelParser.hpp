39b340fba19c354cb5847acaf9728b73898bd414
RyumaNoma <noma11.develop@gmail.com>
2023-09-09 13:23:34 +0900

    [doc] 関数コメントの追加

--- Onigokko/ModelParser.hpp
+++ Onigokko/ModelParser.hpp
@@ -4,12 +4,34 @@
 #include <vector>
 #include <tuple>
 
+namespace game {
 class ModelParser {
 public:
+		/**
+		 * コンストラクタ。
+		 */
 	ModelParser();
+
+		/**
+		 * モデル用ファイルを読み込むコンストラクタ。
+		 * コンストラクタでパースも行う。
+		 *
+		 * @param filename モデル用ファイル
+		 */
 	ModelParser(const std::string& filename);
 
+		/**
+		 * 頂点バッファの参照を取得する。
+		 *
+		 * @return  頂点バッファの参照
+		 */
 	const std::vector<VERTEX3D>& getVertexBuffer() const { return _vertexBuffer; }
+
+		/**
+		 * インデックスバッファの参照を取得する。
+		 *
+		 * @return  インデックスバッファの参照
+		 */
 	const std::vector<std::vector<unsigned short>>& getIndexBuffer() const { return _indexBuffer; }
 		/**
 		 * 頂点数の取得。
@@ -30,3 +52,4 @@ private:
 	std::vector<VERTEX3D> _vertexBuffer;
 	std::vector<std::vector<unsigned short>> _indexBuffer;
 };
+}
