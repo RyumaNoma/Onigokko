6fd4bcf79e4627eab6d86f2a51dc54381e6f4eae
RyumaNoma <noma11.develop@gmail.com>
2023-09-10 13:11:06 +0900

    [change] ModelResourceをgame名前空間に移動

--- Onigokko/ModelResource.hpp
+++ Onigokko/ModelResource.hpp
@@ -8,73 +8,87 @@
  * @details テクスチャ貼り付け機能なし
  * @details 画像の透明度は無効で固定
  */
-class ModelResource {
-public:
-	/**
-	 * @brief デフォルトコンストラクタ。全て空で初期化される
-	 */
-	ModelResource();
+namespace game {
+	class ModelResource {
+	public:
+		/**
+		 * @brief デフォルトコンストラクタ。全て空で初期化される
+		 */
+		ModelResource();
 
-	/**
-	 * @brief コンストラクタ。モデル用ファイルによって初期化を行う。
-	 * 
-	 * @param filepath モデル用ファイル
-	 */
-	ModelResource(std::string filepath);
+		/**
+		 * @brief コンストラクタ。モデル用ファイルによって初期化を行う。
+		 *
+		 * @param filepath モデル用ファイル
+		 */
+		ModelResource(std::string filepath);
 
-	/**
-	 * @brief デストラクタ。
-	 */
-	~ModelResource();
+		/**
+		 * @brief コピーコンストラクタ。
+		 * 
+		 * @param mr コピー元
+		 */
+		ModelResource(const ModelResource& mr);
 
-	/**
-	 * @brief 既に設定されたファイルからのリロード。
-	 * @brief ホットリロードに使われることを想定している。
-	 * @throw std::runtime_error("could not open file") 何らかの理由でファイルを開けなかった場合
-	 */
-	void reload();
+		/**
+		 * @brief 代入演算子。
+		 */
+		ModelResource& operator= (const ModelResource& mr);
 
-	/**
-	 * @brief モデル用ファイルのパスを設定する。
-	 * 
-	 * @param filepath モデル用ファイル
-	 */
-	void setFilepath(const std::string& filepath);
+		/**
+		 * @brief デストラクタ。
+		 */
+		~ModelResource();
 
-	/**
-	 * @brief 頂点バッファの取得。
-	 * 
-	 * @return  頂点バッファの先頭アドレス
-	 */
-	VERTEX3D* getVertexBuffer() const { return _vertexBuffer; }
+		/**
+		 * @brief 既に設定されたファイルからのリロード。
+		 * @brief ホットリロードに使われることを想定している。
+		 * @throw std::runtime_error("could not open file") 何らかの理由でファイルを開けなかった場合
+		 */
+		void reload();
 
-	/**
-	 * @brief 頂点数の取得。
-	 * 
-	 * @return  頂点数
-	 */
-	int getVertexNum() const { return _vertexNum; }
+		/**
+		 * @brief モデル用ファイルのパスを設定する。
+		 *
+		 * @param filepath モデル用ファイル
+		 */
+		void setFilepath(const std::string& filepath);
 
-	/**
-	 * @brief インデックスバッファの取得。
-	 * 
-	 * @return  インデックスバッファの先頭アドレス
-	 */
-	unsigned short* getIndexBuffer() const { return _indexBuffer; }
+		/**
+		 * @brief 頂点バッファの取得。
+		 *
+		 * @return  頂点バッファの先頭アドレス
+		 */
+		VERTEX3D* getVertexBuffer() const { return _vertexBuffer; }
 
-	/**
-	 * @brief ポリゴン数の取得。
-	 * 
-	 * @return  ポリゴン数
-	 */
-	int getPolygonNum() const { return _polygonNum; }
-private:
-	void destroy();
-	void load();
-private:
-	std::string _filepath;
-	VERTEX3D* _vertexBuffer;
-	int _vertexNum;
-	unsigned short* _indexBuffer;
-	int _polygonNum;
-};
+		/**
+		 * @brief 頂点数の取得。
+		 *
+		 * @return  頂点数
+		 */
+		int getVertexNum() const { return _vertexNum; }
+
+		/**
+		 * @brief インデックスバッファの取得。
+		 *
+		 * @return  インデックスバッファの先頭アドレス
+		 */
+		unsigned short* getIndexBuffer() const { return _indexBuffer; }
+
+		/**
+		 * @brief ポリゴン数の取得。
+		 *
+		 * @return  ポリゴン数
+		 */
+		int getPolygonNum() const { return _polygonNum; }
+	private:
+		void destroy();
+		void load();
+	private:
+		std::string _filepath;
+		VERTEX3D* _vertexBuffer;
+		int _vertexNum;
+		unsigned short* _indexBuffer;
+		int _polygonNum;
+	};
+}
