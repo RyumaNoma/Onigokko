110cb33fc7a6981b2d3fc9995b177fca22ef6ca4
RyumaNoma <noma11.develop@gmail.com>
2023-09-11 17:20:20 +0900

    [delete] 不要なデバッグ処理を削除

--- Onigokko/ModelInstance.cpp
+++ Onigokko/ModelInstance.cpp
@@ -4,14 +4,6 @@
 #include <stdexcept>
 #include <fstream>
 
-std::ostream& operator << (std::ostream& os, const DxLib::MATRIX& mat) {
-	os << "(" << mat.m[0][0] << "," << mat.m[0][1] << "," << mat.m[0][2] << "," << mat.m[0][3] << ")" << std::endl
-		<< "(" << mat.m[1][0] << "," << mat.m[1][1] << "," << mat.m[1][2] << "," << mat.m[1][3] << ")" << std::endl
-		<< "(" << mat.m[2][0] << "," << mat.m[2][1] << "," << mat.m[2][2] << "," << mat.m[2][3] << ")" << std::endl
-		<< "(" << mat.m[3][0] << "," << mat.m[3][1] << "," << mat.m[3][2] << "," << mat.m[3][3] << ")" << std::endl;
-	return os;
-}
-
 namespace game {
 	ModelInstance::ModelInstance(ModelResourceRef modelResource)
 		: _scale(VGet(1.0f, 1.0f, 1.0f))
