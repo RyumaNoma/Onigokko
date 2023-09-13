fc863d7a7cccd9eab547585d40e7ef45f1db8d57
RyumaNoma <noma11.develop@gmail.com>
2023-09-13 11:11:16 +0900

    [add] Sceneクラスのインタフェースを作成

--- Onigokko/Scene.cpp
+++
@@ -1,4 +0,0 @@
-#include "Scene.hpp"
-
-namespace game {
-}
--- Onigokko/Scene.hpp
+++ Onigokko/Scene.hpp
@@ -1,8 +1,25 @@
 #pragma once
+#include <memory>
+#include "MessageCommunicator.hpp"
 
 namespace game {
-	class Scene {
+	class Scene
+		: public MessageCommunicator {
+	public:
+		Scene(MessageManagerPtr messageManager) : MessageCommunicator(messageManager) {}
 
+		virtual ~Scene() {}
+
+		/**
+		 * @brief 描画関数。
+		 */
+		virtual void draw() const = 0;
+
+		/**
+		 * @brief 更新関数。
+		 */
+		virtual void update() = 0;
 	};
+	using ScenePtr = std::shared_ptr<Scene>;
 }
 
