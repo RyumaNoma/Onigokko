12e34ba9b89c2985bb42131cfd90a85f30380a08
RyumaNoma <noma11.develop@gmail.com>
2023-09-13 15:49:56 +0900

    [impl] InGameInputKeyboardの実装

--- Onigokko/InGameInputKeyboard.hpp
+++ Onigokko/InGameInputKeyboard.hpp
@@ -1,16 +1,38 @@
-#pragma once
+﻿#pragma once
 #include "InGameInputInterface.hpp"
 
 namespace game {
 	class InGameInputKeyboard
 		: public InGameInputInterface {
 	public:
+		/**
+		 * @brief WASDで8方向移動。
+		 * 
+		 * @return  移動方向
+		 */
 		virtual MOVE_DIRECTION move() override;
 
+		/**
+		 * @brief WASDで8方向移動。
+		 * 
+		 * @return  移動方向
+		 */
 		virtual Direction getDirection() override;
 
+		/**
+		 * @brief 右クリックでアイテム取得。
+		 * 
+		 * @return  アイテム取得ならtrue
+		 */
 		virtual bool getSkillItem() override;
 
+		/**
+		 * @brief 左クリックでアイテム使用。
+		 * 
+		 * @return  アイテム使用ならtrue
+		 */
 		virtual bool useSkillItem() override;
+	private:
+		Direction getDirectionWASD();
 	};
 }
