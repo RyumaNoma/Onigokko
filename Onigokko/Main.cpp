9d73740a7db4c0132d7ebc19084be9f66597d092
RyumaNoma <noma11.develop@gmail.com>
2023-09-13 16:17:27 +0900

    [change] Main.cppをキーボードの使用を試せるようにした

--- Onigokko/Main.cpp
+++ Onigokko/Main.cpp
@@ -7,6 +7,7 @@
 #include "DxLib.h"
 #include "ModelResource.hpp"
 #include "ModelInstance.hpp"
+#include "InGameInputKeyboard.hpp"
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -27,6 +28,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	mi.setScale(100);
 	mi.setAnchor(VGet(0.5, 0.5, 0.5));
 	mi.move(VGet(-0.5, -0.5, -0.5));
+
+	game::InGameInputKeyboard keyboard;
+
 	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
 		SetCameraPositionAndTarget_UpVecY(VGet(0, 400, 400), VGet(0, 0, 0));
 		SetCameraNearFar(5, 1050);
@@ -34,7 +38,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		ClearDrawScreen();
 
 		mi.draw();
-		mi.rotate(DX_PI_F / 100.0f);
+		switch (keyboard.move()) {
+		case game::InGameInputInterface::MOVE_DIRECTION::LEFT:
+			mi.rotate(DX_PI_F / 100.0f);
+			break;
+		case game::InGameInputInterface::MOVE_DIRECTION::RIGHT:
+			mi.rotate(-DX_PI_F / 100.0f);
+			break;
+		case game::InGameInputInterface::MOVE_DIRECTION::UP:
+			mi.move(VGet(0, 1, 0));
+			break;
+		case game::InGameInputInterface::MOVE_DIRECTION::DOWN:
+			mi.move(VGet(0, -1, 0));
+			break;
+		}
 		ScreenFlip();
 	}
 
