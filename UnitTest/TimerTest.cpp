4d47359f267ac55e575f45e9b40ccd494a24249b
RyumaNoma <noma11.develop@gmail.com>
2023-09-15 13:55:15 +0900

    [test][impl] Timerのテストを実装

--- UnitTest/TimerTest.cpp
+++ UnitTest/TimerTest.cpp
@@ -6,5 +6,56 @@ namespace game {
 	TEST_CLASS(TimerTest)
 	{
 	public:
+		TEST_METHOD(Constructor) {
+			ClockPtr clock(new Clock());
+			Timer tm(clock);
+
+			Assert::AreEqual(0ULL, tm.getElapsed());
+		}
+		TEST_METHOD(ElapsedZero) {
+			ClockPtr clock(new Clock());
+			Timer tm(clock);
+			tm.start();
+
+			Assert::AreEqual(0ULL, tm.getElapsed());
+		}
+		TEST_METHOD(ElapsedOne) {
+			ClockPtr clock(new Clock());
+			Timer tm(clock);
+
+			clock->countUp();
+
+			Assert::AreEqual(1ULL, tm.getElapsed());
+		}
+		TEST_METHOD(ElapsedMany) {
+			ClockPtr clock(new Clock());
+			Timer tm(clock);
+
+			clock->countUp();
+			clock->countUp();
+			clock->countUp();
+			clock->countUp();
+			clock->countUp();
+
+			Assert::AreEqual(5ULL, tm.getElapsed());
+		}
+		TEST_METHOD(Restart) {
+			ClockPtr clock(new Clock());
+			Timer tm(clock);
+
+			clock->countUp();
+			clock->countUp();
+			clock->countUp();
+			Assert::AreEqual(3ULL, tm.getElapsed());
+
+			tm.start();
+			Assert::AreEqual(0ULL, tm.getElapsed());
+
+			clock->countUp();
+			clock->countUp();
+			clock->countUp();
+			clock->countUp();
+			Assert::AreEqual(4ULL, tm.getElapsed());
+		}
 	};
 }
