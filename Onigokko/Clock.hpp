a6b4ee9b5d3ebd2a4716a18c5497fca8abaa8f2d
RyumaNoma <noma11.develop@gmail.com>
2023-09-15 13:20:06 +0900

    [add] Clockクラスのヘッダ定義

--- Onigokko/Clock.hpp
+++ Onigokko/Clock.hpp
@@ -1,6 +1,30 @@
-#pragma once
+﻿#pragma once
 
 namespace game {
+	/**
+	 * @brief フレーム数に準拠した時間管理クラス。
+	 * @attention フレーム数がsize_tの範囲を超えることはないものとして実装する
+	 */
 	class Clock {
+	public:
+		/**
+		 * @brief コンストラクタ。時刻0で初期化される
+		 */
+		Clock();
+
+		/**
+		 * @brief 時刻を1進める。
+		 * 
+		 */
+		void countUp();
+
+		/**
+		 * @brief 現在の時刻を取得する。
+		 * 
+		 * @return  現在の時刻
+		 */
+		size_t now() const { return _now; }
+	private:
+		size_t _now;
 	};
 }
