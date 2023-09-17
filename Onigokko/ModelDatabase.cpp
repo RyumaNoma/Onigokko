31a165f685d081bdb19c949cfa3355b77001d6b1
RyumaNoma <noma11.develop@gmail.com>
2023-09-18 01:15:44 +0900

    [impl] ModelDatabaseの実装

--- Onigokko/ModelDatabase.cpp
+++ Onigokko/ModelDatabase.cpp
@@ -1,4 +1,20 @@
 ﻿#include "ModelDatabase.hpp"
+#include "ModelResource.hpp"
 
 namespace game {
+	ModelDatabase::ModelDatabase()
+		: _resources()
+	{}
+	
+	void ModelDatabase::load(const std::string & filename) {
+		ModelResourcePtr model(new ModelResource(filename));
+		_resources.emplace(filename, model);
+	}
+	
+	ModelResourcePtr ModelDatabase::fetch(const std::string& filename) {
+		if (_resources.find(filename) == _resources.end()) {
+			load(filename);
+		}
+		return _resources.at(filename);
+	}
 }
