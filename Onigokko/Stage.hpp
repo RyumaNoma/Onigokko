53e21b2a4999c8744b719d873c260216a8184c4b
RyumaNoma <noma11.develop@gmail.com>
2023-09-22 00:19:31 +0900

    [add] Stageクラスのヘッダに定義を追加

--- Onigokko/Stage.hpp
+++ Onigokko/Stage.hpp
@@ -1,7 +1,74 @@
 ﻿#pragma once
+#include <memory>
+#include <vector>
+
+namespace DxLib {
+	class VECTOR;
+}
 
 namespace game {
-	class Stage {
+	class ModelDatabase;
+	using ModelDatabaseRef = std::shared_ptr<const ModelDatabase>;
 
+	class ModelInstance;
+	using ModelInstancePtr = std::shared_ptr<ModelInstance>;
+	using ModelInstanceRef = std::shared_ptr<const ModelInstance>;
+
+	/**
+	 * @brief ゲーム内のステージを管理する。
+	 */
+	class Stage {
+	public:
+		/**
+		 * @brief デフォルトコンストラクタ。
+		 * @brief 床と壁4枚が生成される。
+		 * 
+		 * @param scale ステージの大きさ
+		 */
+		Stage(DxLib::VECTOR scale);
+
+		/**
+		 * @brief モデルDBを通したステージの生成。
+		 * @brief 床と壁4枚が生成される。
+		 * 
+		 * @param modelDatabase 元となるモデルDB
+		 * @param scale ステージの大きさ
+		 */
+		Stage(ModelDatabaseRef modelDatabase, DxLib::VECTOR scale);
+
+		/**
+		 * @brief 床と壁4枚を生成する。
+		 * 
+		 * @param scale ステージの大きさ
+		 */
+		void init(DxLib::VECTOR scale);
+
+		/**
+		 * @brief ステージの持つ全てのオブジェクトの描画。
+		 */
+		void draw() const;
+
+		/**
+		 * @brief 床オブジェクトの取得。
+		 * 
+		 * @return  床オブジェクト
+		 */
+		const std::vector<ModelInstancePtr>& getFloors() const { return _floors; }
+
+		/**
+		 * @brief 壁オブジェクトの取得。
+		 * 
+		 * @return  壁オブジェクト
+		 */
+		const std::vector<ModelInstancePtr>& getWalls() const { return _walls; }
+
+		/**
+		 * @brief ステージの持つすべてのオブジェクトの取得。
+		 * 
+		 * @return  ステージの持つすべてのオブジェクト（床・壁）
+		 */
+		std::vector<ModelInstanceRef> getAllObjects() const;
+	private:
+		std::vector<ModelInstancePtr> _floors, _walls;
 	};
 }
