84ded4181d7dbb5335975cf5b99c6ae6a69a8301
RyumaNoma <noma11.develop@gmail.com>
2023-09-23 19:13:50 +0900

    [add] AABBのヘッダの定義を作成

--- Onigokko/AABB.hpp
+++ Onigokko/AABB.hpp
@@ -1,7 +1,35 @@
 ﻿#pragma once
+#include <memory>
+#include "DxLib.h"
 
 namespace game {
+	class ModelInstance;
+	using ModelInstanceRef = std::shared_ptr<const ModelInstance>;
+
+	/**
+	 * @brief AABB(axis-aligned bounding boxes)
+	 */
 	class AABB {
 	public:
+		AABB();
+
+		void update(ModelInstanceRef model);
+
+		void update(VECTOR min, VECTOR max);
+
+		bool intersect(const AABB& aabb) const;
+
+		void drawFrame() const;
+
+		float getMinX() const { return minX; }
+		float getMaxX() const { return maxX; }
+		float getMinY() const { return minY; }
+		float getMaxY() const { return maxY; }
+		float getMinZ() const { return minZ; }
+		float getMaxZ() const { return maxZ; }
+	private:
+		float minX, maxX;
+		float minY, maxY;
+		float minZ, maxZ;
 	};
 }
