3ca51a20a6f70e0bc1d8f1a946a00edd3eaadf0b
RyumaNoma <noma11.develop@gmail.com>
2023-09-24 23:52:56 +0900

    [test] AABBのdrawFrameをテストできるようにMain関数を変更

--- Onigokko/Main.cpp
+++ Onigokko/Main.cpp
@@ -7,8 +7,8 @@
 #include "DxLib.h"
 #include "ModelResource.hpp"
 #include "ModelInstance.hpp"
-#include "ModelDatabase.hpp"
-#include "Stage.hpp"
+#include "AABB.hpp"
+#include "InGameInputPad.hpp"
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -19,31 +19,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	SetBackgroundColor(128, 128, 128);
-	SetUseLighting(true);
+	SetUseLighting(false);
 	SetUseZBuffer3D(true);
 	SetWriteZBuffer3D(true);
 	SetDrawScreen(DX_SCREEN_BACK);
 
-
-	game::ModelDatabasePtr mdb(new game::ModelDatabase());
-	mdb->load("floor", "ground.txt");
-	mdb->load("wall", "wall.txt");
-	game::Stage stage(VGet(100, 100, 100),
-			"ground.txt",
-			"wall.txt");
+	game::ModelResourcePtr mr(new game::ModelResource("item.txt"));
+	std::shared_ptr<game::ModelInstance> mi(new game::ModelInstance(mr));
+	mi->setScale(100);
+	game::AABB aabb;
 
 	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
-		SetCameraPositionAndTarget_UpVecY(VGet(0, 200, 200), VGet(50, 0, 50));
+		SetCameraPositionAndTarget_UpVecY(VGet(400, 400, 0), VGet(100, 0, 1));
 		SetCameraNearFar(5, 1050);
 
 		ClearDrawScreen();
 
-		stage.draw();
+		mi->rotate(DX_PI_F / 300);
+		aabb.update(mi);
+		mi->draw();
+		aabb.drawFrame();
 
 		ScreenFlip();
 	}
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
 
-	return 0;				// ソフトの終了 
+	return 0;				// ソフトの終了
 }
