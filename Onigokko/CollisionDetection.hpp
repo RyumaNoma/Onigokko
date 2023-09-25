dce1d2eb3482f0b4517faf772a7cbdbb9814f066
RyumaNoma <noma11.develop@gmail.com>
2023-09-25 18:39:12 +0900

    [impl] 運動状態のAABB同士の衝突検出を実装

--- Onigokko/CollisionDetection.hpp
+++ Onigokko/CollisionDetection.hpp
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include "DxLib.h"
 
 namespace game {
 	class AABB;
@@ -8,6 +9,36 @@ namespace game {
 	 */
 	class CollisionDetection {
 	public:
+		/**
+		 * @brief 静止状態のAABB同士の衝突検出。
+		 * 
+		 * @param aabb1 静止状態のAABB
+		 * @param aabb2 静止状態のAABB
+		 * @return 衝突しているならtrue
+		 */
 		static bool testStop(const AABB& aabb1, const AABB& aabb2);
+
+		/**
+		 * @brief 運動状態のAABB同士の衝突検出。
+		 * 
+		 * @param aabb1 運動状態のAABB
+		 * @param velocity1 aabb1の速度ベクトル
+		 * @param aabb2 運動状態のAABB
+		 * @param velocity2 aabb2の速度ベクトル
+		 * @return 衝突しているならtrue
+		 */
+		static bool testMove(
+			const AABB& aabb1, VECTOR velocity1,
+			const AABB& aabb2, VECTOR velocity2
+		);
+		/**
+		 * @brief ベクトル形式の線分と静止状態のAABBの衝突検出。
+		 * 
+		 * @param origin 線分の始点
+		 * @param dir 線分のベクトル
+		 * @param aabb 静止状態のAABB
+		 * @return  衝突しているならtrue
+		 */
+		static bool testVectorAndAABB(const VECTOR origin, const VECTOR dir, const AABB& aabb);
 	};
 }
