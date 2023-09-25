1a482f5ad20b9d2d13fd487632f73ce1662467ab
RyumaNoma <noma11.develop@gmail.com>
2023-09-25 11:25:25 +0900

    [add] 衝突していない場合のテストケースを追加

--- UnitTest/CollisionDetectionTest.cpp
+++ UnitTest/CollisionDetectionTest.cpp
@@ -48,5 +48,54 @@ namespace game {
 			Assert::IsTrue(CollisionDetection::testStop(a, b));
 			Assert::IsTrue(CollisionDetection::testStop(b, a));
 		}
+		TEST_METHOD(StopNotCollision) {
+			AABB a, b;
+			a.update(VGet(30, 30, 30), VGet(50, 50, 50));
+			b.update(VGet(60, 60, 60), VGet(70, 70, 70));
+			Assert::IsFalse(CollisionDetection::testStop(a, b));
+			Assert::IsFalse(CollisionDetection::testStop(b, a));
+		}
+		TEST_METHOD(StopNotCollisionX) {
+			AABB a, b;
+			a.update(VGet(0, -20, -20), VGet(10, 10, 10));
+			b.update(VGet(100, -5, -5), VGet(1000, 40, 40));
+			Assert::IsFalse(CollisionDetection::testStop(a, b));
+			Assert::IsFalse(CollisionDetection::testStop(b, a));
+		}
+		TEST_METHOD(StopNotCollisionY) {
+			AABB a, b;
+			a.update(VGet(-20, 0, -20), VGet(10, 10, 10));
+			b.update(VGet(-5, 100, -5), VGet(40, 1000, 40));
+			Assert::IsFalse(CollisionDetection::testStop(a, b));
+			Assert::IsFalse(CollisionDetection::testStop(b, a));
+		}
+		TEST_METHOD(StopNotCollisionZ) {
+			AABB a, b;
+			a.update(VGet(-20, -20, 0), VGet(10, 10, 10));
+			b.update(VGet(-5, -5, 100), VGet(40, 40, 1000));
+			Assert::IsFalse(CollisionDetection::testStop(a, b));
+			Assert::IsFalse(CollisionDetection::testStop(b, a));
+		}
+		TEST_METHOD(StopCollisionOnlyPoint) {
+			AABB a, b;
+			a.update(VGet(30, 30, 30), VGet(50, 50, 50));
+			b.update(VGet(50, 50, 50), VGet(70, 70, 70));
+			Assert::IsFalse(CollisionDetection::testStop(a, b));
+			Assert::IsFalse(CollisionDetection::testStop(b, a));
+		}
+		TEST_METHOD(StopCollisionOnlyLine) {
+			AABB a, b;
+			a.update(VGet(0, 0, 0), VGet(50, 50, 50));
+			b.update(VGet(50, 50, 20), VGet(70, 70, 70));
+			Assert::IsFalse(CollisionDetection::testStop(a, b));
+			Assert::IsFalse(CollisionDetection::testStop(b, a));
+		}
+		TEST_METHOD(StopCollisionOnlySurface) {
+			AABB a, b;
+			a.update(VGet(0, 0, 0), VGet(50, 50, 50));
+			b.update(VGet(20, 20, 50), VGet(70, 70, 70));
+			Assert::IsFalse(CollisionDetection::testStop(a, b));
+			Assert::IsFalse(CollisionDetection::testStop(b, a));
+		}
 	};
 }
