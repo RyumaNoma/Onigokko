22e83c4c3574b3c12c480c18905a66707b2186d6
RyumaNoma <noma11.develop@gmail.com>
2023-09-25 11:08:16 +0900

    [add] 静止時の衝突検出用関数の定義を追加

--- Onigokko/CollisionDetection.hpp
+++ Onigokko/CollisionDetection.hpp
@@ -1,7 +1,13 @@
 ﻿#pragma once
 
 namespace game {
-	class CollisionDetection {
+	class AABB;
 
+	/**
+	 * @brief 衝突検出用関数群。
+	 */
+	class CollisionDetection {
+	public:
+		static bool testStop(const AABB& aabb1, const AABB& aabb2);
 	};
 }
