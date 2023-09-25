d68c8e18f8f4823d816999d7ca4b71799eb6d891
RyumaNoma <noma11.develop@gmail.com>
2023-09-25 11:10:16 +0900

    [impl] 静止時の関数を実装

--- Onigokko/CollisionDetection.cpp
+++ Onigokko/CollisionDetection.cpp
@@ -1,4 +1,11 @@
 ﻿#include "CollisionDetection.hpp"
+#include "AABB.hpp"
 
 namespace game {
+	bool CollisionDetection::testStop(const AABB& aabb1, const AABB& aabb2) {
+		const bool collisionX = (aabb1.getMaxX() > aabb2.getMinX()) && (aabb1.getMinX() < aabb2.getMaxX());
+		const bool collisionY = (aabb1.getMaxY() > aabb2.getMinY()) && (aabb1.getMinY() < aabb2.getMaxY());
+		const bool collisionZ = (aabb1.getMaxZ() > aabb2.getMinZ()) && (aabb1.getMinZ() < aabb2.getMaxZ());
+		return (collisionX && collisionY && collisionZ);
+	}
 }
