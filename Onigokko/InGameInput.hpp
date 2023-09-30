0c31fc96f8291f58a757e2f4bbd81685800309ed
RyumaNoma <noma11.develop@gmail.com>
2023-09-30 15:39:20 +0900

    [add] InGameInputのヘッダ定義を追加

--- Onigokko/InGameInput.hpp
+++ Onigokko/InGameInput.hpp
@@ -1,7 +1,25 @@
 #pragma once
+#include <memory>
+#include <vector>
+#include "MessageClient.hpp"
 
 namespace game {
-	class InGameInput {
+	class InGameInputInterface;
+	using InGameInputInterfacePtr = std::shared_ptr<InGameInputInterface>;
+
+	class InGameInput
+		: public MessageClient
+	{
+	public:
+		InGameInput(MessageServerPtr messageServer);
+		virtual void receive(const std::string& message) override;
+
+		// 入力機器のセット
+		void setDevice(int playerId, InGameInputInterfacePtr device);
+		InGameInputInterfacePtr getDevice(int playerId) const;
+		size_t size() const { return _inputDevices.size(); }
+	private:
+		std::vector<InGameInputInterfacePtr> _inputDevices;
 	};
 }
 
