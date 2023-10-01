ea976015dce70300ed28e2b8be5bc088ffdf6c85
RyumaNoma <noma11.develop@gmail.com>
2023-10-01 10:58:38 +0900

    [add] Playerクラスのヘッダ定義を追加

--- Onigokko/Player.hpp
+++ Onigokko/Player.hpp
@@ -1,5 +1,42 @@
 ﻿#pragma once
-class Player
-{
-};
 
+namespace game {
+	/**
+	 * @brief インゲームのプレイヤークラス。
+	 */
+	class Player {
+	public:
+		enum class STATUS {
+			NONE,
+			ONI,
+			PERSON
+		};
+		Player() = delete;
+
+		/**
+		 * @brief コンストラクタ。
+		 * 
+		 * @param oniSpeed 鬼状態の速度
+		 * @param personSpeed 人間状態のそ度
+		 */
+		Player(float oniSpeed, float personSpeed);
+
+		/**
+		 * @brief 鬼になる。
+		 */
+		void beOni();
+
+		/**
+		 * @brief 人間になる。
+		 */
+		void bePerson();
+
+		STATUS getStatus() const { return _status; }
+		float getSpeed() const { return _speed; }
+	private:
+		STATUS _status;
+		float _speed;
+		const float _oniSpeed;
+		const float _personSpeed;
+	};
+}
