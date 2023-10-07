eabb5d9e14b57eef9481e5f8eab2f67e87b08069
RyumaNoma <noma11.develop@gmail.com>
2023-10-07 10:16:50 +0900

    [add] Directionのヘッダ定義を追加

---
+++ Onigokko/Direction.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <string>
+
+namespace game {
+	/**
+	 * @brief 2次元のスティック入力の方向。
+	 * @attention x(右方向が+)、y(上方向が+)
+	 */
+	class Direction {
+	public:
+		Direction();
+		Direction(int x, int y);
+
+		explicit operator std::string() const;
+
+		int x() const { return _x; }
+		int y() const { return _y; }
+	private:
+		int _x, _y;
+	};
+}
